067daf9f792d73e5f313b39afd3de6d5f5988a8b
Robin Forbes <rsforbes0@gmail.com>
2024-10-15 23:05:07 +0100

    dump model config; fix parallel statuses

--- scripts/stat/StatExt.py
+++ scripts/stat/StatExt.py
@@ -1,3 +1,5 @@
+import json
+import pprint
 import re
 from extUtils import CustomParHelper
 from transitions.extensions import HierarchicalMachine
@@ -42,34 +44,48 @@ class StatExt:
         event = self.evalEvent
         self.Machine.dispatch(event)
 
+    def DumpSchema(self, config):
+        text = op("text_debug")
+        text.text = pprint.pformat(config, indent=4)
+
     def Collect(self):
         # preserve the current state if it still exists
         preserve = self.state
-        self.Machine = HierarchicalMachine(after_state_change=self.after_state_change)
         ops = op("opfind1").rows(val=True)
         debug("ops", ops)
+
+        config = {
+            "states": [],
+            "transitions": [],
+            "after_state_change": self.after_state_change,
+        }
+
         initial = None
         for name, type in ops[1:]:
             type = type[1:-1]  # remove quotes
             if type == "stat_state":
-                debug("adding state from op", name)
-                added = self.add_state_from_op(name)
-                if added and initial is None:
+                s: "StateExt" = op(name)
+                state = s.GetConfig()
+                if state is None:
+                    continue
+
+                if initial is None:
                     initial = name
+                config["states"].append(state)
             elif type == "stat_transition":
-                debug("adding transition from op", name)
-                self.add_transition_from_op(name)
+                t: "TransitionExt" = op(name)
+                transitions = t.GetConfig()
+                if isinstance(transitions, list):
+                    config["transitions"].extend(transitions)
+                else:
+                    config["transitions"].append(transitions)
+
+        debug("Collected config: ", config)
+        if self.evalShowschema:
+            self.DumpSchema(config)
+        self.Machine = HierarchicalMachine(**config)
 
         self.SetState(preserve if preserve != "initial" else initial)
-
-    def add_state_from_op(self, name):
-        s: "StateExt" = op(name)
-        state = s.GetConfig()
-        debug("state config: ", state)
-        if state is None:
-            return False
-        self.Machine.add_state(state)
-        return True
 
     def add_transition_from_op(self, name):
         t: "TransitionExt" = op(name)
