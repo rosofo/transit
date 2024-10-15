faea34600ff3ed438cdc00ec2f08640669aeebae
Robin Forbes <rsforbes0@gmail.com>
2024-10-15 10:51:31 +0100

    auto collect; working example

--- scripts/stat/StatExt.py
+++ scripts/stat/StatExt.py
@@ -6,16 +6,24 @@ from transitions import Machine
 class StatExt:
     def __init__(self, ownerComp):
         self.ownerComp = ownerComp
-        self.Machine = Machine(self)
+        self.Machine = Machine()
         CustomParHelper.Init(
             self, ownerComp, enable_properties=True, enable_callbacks=True
         )
         self.State = tdu.Dependency(val=self.state)
+        self.Collect()
+
+    @property
+    def state(self):
+        return self.Machine.get_model_state(self.Machine).name
 
     def onParCollect(self):
         self.Collect()
 
     def onParState(self, state):
+        self.SetState(state)
+
+    def SetState(self, state):
         self.Machine.set_state(state)
         self.State.val = state
 
@@ -24,18 +32,25 @@ class StatExt:
         self.Machine.dispatch(event)
 
     def Collect(self):
-        self.Machine = Machine(self, after_state_change=self.after_state_change)
+        # preserve the current state if it still exists
+        preserve = self.state
+        self.Machine = Machine(after_state_change=self.after_state_change)
 
         ops = op("opfind1").rows(val=True)
         debug("ops", ops)
+        initial = None
         for name, type in ops[1:]:
             type = type[1:-1]  # remove quotes
             if type == "stat_state":
                 debug("adding state", name)
                 self.Machine.add_state(name)
+                if initial is None:
+                    initial = name
             elif type == "stat_transition":
                 debug("adding transition from op", name)
                 self.add_transition_from_op(name)
+
+        self.SetState(preserve if preserve != "initial" else initial)
 
     def add_transition_from_op(self, name):
         t: "TransitionExt" = op(name)
@@ -44,4 +59,5 @@ class StatExt:
         self.Machine.add_transitions(trans)
 
     def after_state_change(self):
+        debug("after state change", self.state)
         self.State.val = self.state
--- scripts/transition/TransitionExt.py
+++ scripts/transition/TransitionExt.py
@@ -14,32 +14,42 @@ class TransitionExt:
         self,
     ) -> "transitions.core.TransitionConfig | list[transitions.core.TransitionConfig]":
         inlets: "list[Connector]" = self.ownerComp.inputConnectors
-        in_states: list["OP"] = [
+        in_ops: list["OP"] = [
             i.connections[0].owner
             for i in inlets
             if i.connections and i.connections[0].owner
         ]
+        in_states = [op for op in in_ops if "stat_state" in op.tags]
+        in_conds = [
+            lambda: op.par.Valid.eval() == 1
+            for op in in_ops
+            if "stat_condition" in op.tags
+        ]
+
         outlets = self.ownerComp.outputConnectors
         out_states = [
-            i.connections[0].owner
+            conn.owner
             for i in outlets
             if i.connections and i.connections[0].owner
+            for conn in i.connections
         ]
         event = self.evalEvent
 
         sources = [s.name for s in in_states]
         targets = [t.name for t in out_states]
         ts = []
-        for source, target in product(sources, targets):
+        for target in targets:
             ts.append(
                 {
-                    "source": source,
+                    "source": sources,
                     "dest": target,
                     "trigger": event,
                     "after": self.on_after,
+                    "conditions": in_conds,
                 }
             )
         return ts
 
     def on_after(self):
         op("trigger_after").par.triggerpulse.pulse()
+        self.ownerComp.par.Transitioned.pulse()
