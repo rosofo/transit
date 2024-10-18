8ddefadf2b2fd9ddbffe6cb9c4f9bc3cb31aff2d
Robin Forbes <rsforbes0@gmail.com>
2024-10-18 02:00:57 +0100

    presets

--- scripts/Transit.py
+++ scripts/Transit.py
@@ -54,6 +54,7 @@ class Transit:
         page = comp.appendCustomPage(page_name)
         self._expose_state(machine, page)
         self._expose_events(machine, page)
+        self._expose_presets(machine, page)
 
     def _expose_state(self, machine: Machine, page: "Page"):
         par = page.appendStr("State", label="State")
@@ -77,6 +78,30 @@ class Transit:
             self._create_pulse(
                 page=page, callback=dispatch_event, par_name=par_name, label=event
             )
+
+    def _expose_presets(self, machine: Machine, page: "Page"):
+        page.appendSequence("Presets", label="Presets")  # type: ignore
+        p = page.appendFloat("Presetvalue", label="Value")
+        p.readOnly = True
+        for state in machine.states:
+            page.appendFloat(f"Preset{self.to_par_name(state).lower()}", label=state)
+        page.owner.seq.Presets.blockSize = 1 + len(machine.states)
+
+        machine.after_state_change.append(self._create_set_presets(machine))
+
+    def _create_set_presets(self, machine: Machine):
+        def set_presets(*args, **kwargs):
+            comp = self.ext.ownerComp
+            seq: "Sequence" = comp.seq.Presets
+            state = machine.state
+            for i, preset in enumerate(seq.blocks):
+                matching_par = preset.parGroup[
+                    f"Preset{self.to_par_name(state).lower()}"
+                ][0]
+                val = matching_par.eval()
+                preset.par.Presetvalue.val = val
+
+        return set_presets
 
     def _create_pulse(self, page: "Page", par_name, label, callback):
         page.appendPulse(par_name, label=label)
