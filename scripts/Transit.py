6bc68a99c149ba158c826545bf2211eef0607c33
Robin Forbes <rsforbes0@gmail.com>
2024-10-18 01:31:20 +0100

    dynamic conditions

--- scripts/Transit.py
+++ scripts/Transit.py
@@ -1,4 +1,5 @@
-from typing import Protocol
+import re
+from typing import Callable, Protocol
 
 op("td_pip").PrepareModule("transitions")  # type: ignore
 from transitions import Machine  # noqa: E402
@@ -13,16 +14,61 @@ class Transit:
 
     def __init__(self, extension: Extension) -> None:
         self.ext = extension
+        self._reset_custom_pages()
+
+    def _reset_custom_pages(self):
+        comp = self.ext.ownerComp
+        for page in comp.customPages:
+            page.destroy()
+
+    def condition(self, name: str, callback: Callable[[float], bool] | None = None):
+        """Create a condition for use in transitions and expose it as a custom parameter"""
+        comp = self.ext.ownerComp
+        page = next((p for p in comp.customPages if p.name == "Conditions"), None)
+        if page is None:
+            page = comp.appendCustomPage("Conditions")
+
+        par = next((p for p in page.pars if p.name == self.to_par_name(name)), None)
+        if par is None:
+            par = page.appendFloat(self.to_par_name(name), label=name)[0]
+
+        if callback is not None:
+
+            def callback_condition_fn(*args, **kwargs):
+                return callback(par.eval())
+
+            return callback_condition_fn
+        else:
+
+            def condition_fn(*args, **kwargs):
+                return par.eval() >= 1
+
+            return condition_fn
 
     def expose_machine(self, machine: Machine):
-        self.expose_events(machine)
-
-    def expose_events(self, machine: Machine):
+        """Expose states and events of a transitions.Machine as custom parameters"""
         comp = self.ext.ownerComp
-        page = self._recreate_page(comp, "Events")
+        if not machine.name:
+            raise ValueError("Machine must have a name")
+        page_name = f"Machine:{machine.name.capitalize()}"
+        page = comp.appendCustomPage(page_name)
+        self._expose_state(machine, page)
+        self._expose_events(machine, page)
+
+    def _expose_state(self, machine: Machine, page: "Page"):
+        par = page.appendStr("State", label="State")
+        par.readOnly = True
+        par.val = machine.state
+
+        def after_state_change(*args, **kwargs):
+            par.val = machine.state
+
+        machine.after_state_change.append(after_state_change)
+
+    def _expose_events(self, machine: Machine, page: "Page"):
         for event in machine.events:
             debug(f"Exposing event {event}")
-            par_name = self._custom_par_name(event)
+            par_name = self._event_par_name(event)
 
             def dispatch_event(self, *_, event=event):
                 debug(f"Dispatching event {event}")
@@ -45,15 +91,9 @@ class Transit:
         setattr(self.ext.__class__, method_name, method)
 
     @staticmethod
-    def _recreate_page(comp: "COMP", name: str):
-        page = next(
-            (p for p in comp.customPages if p.name.lower() == name.lower()), None
-        )
-        if page is not None:
-            page.destroy()
-        page = comp.appendCustomPage(name)
-        return page
+    def _event_par_name(name: str):
+        return "Dispatch" + Transit.to_par_name(name).lower()
 
     @staticmethod
-    def _custom_par_name(name: str):
-        return "Dispatch" + "".join([w for w in name.lower().split("_")])
+    def to_par_name(name):
+        return "".join([w for w in re.split(r"[\s_]+", name.capitalize())])
--- scripts/transit/TransitExt.py
+++ scripts/transit/TransitExt.py
@@ -1,3 +1,5 @@
+import re
+from typing import Any, Iterable
 from extUtils import CustomParHelper
 from Transit import Transit
 
@@ -12,6 +14,7 @@ class TransitExt:
         self.ownerComp = ownerComp
         self.transit = Transit(self)
         self.Machine = Machine(
+            name="foo",
             states=["A", "B", "C"],
             initial="A",
             transitions=[{"trigger": "go", "source": "A", "dest": "B"}],
@@ -20,3 +23,42 @@ class TransitExt:
         CustomParHelper.Init(
             self, ownerComp, enable_properties=True, enable_callbacks=True
         )
+
+    def IsState(self, *states: str, exact: bool = False):
+        self.ownerComp.par.State.val  # trigger dependency of the caller
+        if exact:
+            return all(self.is_state_exact(state) for state in states)
+        else:
+            return all(self.is_state_partial(state) for state in states)
+
+    def OneOf(self, *states: str, exact: bool = False):
+        return any(self.IsState(s, exact=exact) for s in states)
+
+    def Map(self, expr: str):
+        terms = re.split(r"\s+", expr)
+        if len(terms) % 2 != 0:
+            raise ValueError(
+                "Expression must have an even number of terms, e.g. 'state_a 0 state_b 1'"
+            )
+        pairs = list(zip(terms[::2], terms[1::2]))
+        pairs = [(state, float(value)) for state, value in pairs]
+        return self.Pick(*pairs)
+
+    def Pick(self, *states: str | tuple[str | Iterable[str], Any], exact: bool = False):
+        for i, state in enumerate(states):
+            value = i
+            if isinstance(state, tuple):
+                state, value = state
+            state = (state,) if isinstance(state, str) else state
+            if self.IsState(*state, exact=exact):
+                return value
+        return 0
+
+    def is_state_exact(self, state: str):
+        return self.Machine.is_state(state, self.Machine)
+
+    def is_state_partial(self, state: str):
+        """Handle compound states. E.g. given state `a_b` or `[a, [b, c]]`, `is_state_partial('a') == True`."""
+        # horrible but they can be arbitrarily nested!
+        states_str = str(self.Machine.state)
+        return state in states_str
